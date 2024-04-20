80f6e3993439c4c2d89dd345a14c0da10dee45a9
AdhamKhaledDev <adhamk109@outlook.com>
2024-04-19 18:24:28 -0700

    IaaS_Phase

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,9 @@
+class { 'python': }  # Include the base python class
+
+package { 'Flask':
+  ensure    => installed,
+  provider  => 'pip',
+  require   => Class['python'],
+  name      => 'flask',
+  version   => '2.1.0',
+}
