fe5751c3475f65a9ed9d4b6fd4343e80d082ba29
AdhamKhaledDev <adhamk109@outlook.com>
2024-04-19 18:26:37 -0700

    Puppet_Linter_Edition

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,9 @@
 class { 'python': }  # Include the base python class
 
 package { 'Flask':
-  ensure    => installed,
-  provider  => 'pip',
-  require   => Class['python'],
-  name      => 'flask',
-  version   => '2.1.0',
+  ensure   => installed,
+  provider => 'pip',
+  require  => Class['python'],
+  name     => 'flask',
+  version  => '2.1.0',
 }
