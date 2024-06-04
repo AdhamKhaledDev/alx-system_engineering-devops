345707d00a789264b807fe094ad4ed553586a41d
AdhamKhaledDev <adhamk109@outlook.com>
2024-06-04 11:15:30 -0700

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
+
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
@@ -10,6 +13,4 @@ def number_of_subscribers(subreddit):
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
-    if results is None:
-        return 0
-    return results.get("subscribers", 0)
+    return results.get("subscribers")
