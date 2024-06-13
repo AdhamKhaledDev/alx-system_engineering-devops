63c6ee7c1b948fb97d8f25c77232214c2ca59ce3
AdhamKhaledDev <adhamk109@outlook.com>
2024-06-12 20:56:42 -0700

    pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,12 @@
-#the_sky_is_the_limit_not.pp set open file limit higher
-exec { 'set limit to 2000':
-  path    => '/bin',
-  command => "sed -i 's/15/2000/' /etc/default/nginx"
+# Fix problem of high amount of requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
-exec { 'reboot nginx':
-  command => '/usr/sbin/service nginx restart'
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
